213fb38f7b53c79f0245671bcbe19e43ced05995
misper <victoriuscamver@gmail.com>
2025-06-29 22:13:45 +0200

    Expanded info command into a new command group for viewing of member info

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from lib.checks import *
 # Carga de secretos
 TOKEN = os.getenv('TOKEN')
 PREFIX = '!'
-VERSION = "1.1"
-UP_DATE: datetime.datetime
 
 # Setup de intents (permisos disponibles al bot)
 intents = discord.Intents.default()
@@ -14,23 +12,10 @@ intents.members = True
 # Setup del cliente del bot
 bot = commands.Bot(command_prefix=PREFIX, intents=intents)
 
-@bot.hybrid_command(name="info", description="Devuelve información sobre el bot.")
-async def info(ctx: commands.Context):
-    global UP_DATE
-    embed = discord.Embed(
-        title="Información",
-        color=discord.Colour.green(),
-        timestamp=datetime.datetime.now()
-    )
-    embed.add_field(name="Versión", value=VERSION, inline=False)
-    embed.add_field(name="Ejecutándose desde", value=f"<t:{int(UP_DATE.timestamp())}:R>")
-
-    await ctx.reply(embed=embed, ephemeral=True)
-
 # Logging del bot
 @bot.event
 async def on_ready():
-    cogs = ("cogs.econ", "cogs.gpibe", "cogs.errors", "cogs.config")
+    cogs = ("cogs.econ", "cogs.gpibe", "cogs.errors", "cogs.config", "cogs.info")
     # Carga de módulos del bot
     for cog in cogs:
         try:
@@ -38,14 +23,11 @@ async def on_ready():
             print(f"Módulo {cog} cargado con éxito.")
         except commands.ExtensionFailed:
             print("No se ha podido cargar el módulo.")
-
     os.makedirs("data", exist_ok=True)
     init_file("data/members.json")
     init_file("data/server.json")
     init_file("data/comisiones.json")
     await bot.tree.sync()
-    global UP_DATE
-    UP_DATE = datetime.datetime.now()
     print("Bot Online")
 
 # Inicialización
